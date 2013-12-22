3a4131843c4cfad5c9f593290075c557a0a0d864
Michael Kurtz <michaelkurtz10@gmail.com>
2013-12-21 19:31:33 -0500

    changed config so that DB is hosted on asmallorange

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'michaelkurtz10@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'mikeprac_p3_biz'); 
+	define('DB_NAME', 'mikeprac_p4_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
